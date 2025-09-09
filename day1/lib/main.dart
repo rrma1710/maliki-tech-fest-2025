dfeb37d4c28b4064af1bbc647c5b7e812fe7b1a6
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-09 08:18:04 +0700

    update appbar

--- day1/lib/main.dart
+++ day1/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Study_case_1')),
+        appBar: AppBar(title: Text('Study Case 1 - Day 1')),
         body: Center(child: Text('Test Project')),
       ),
     );
