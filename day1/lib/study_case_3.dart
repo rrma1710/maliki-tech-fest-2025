c6b10f97381bd32053d962ee79508b6083ac4ef9
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-09 23:16:30 +0700

    perubahan bug pada initstate

---
+++ day1/lib/study_case_3.dart
@@ -0,0 +1,57 @@
+import 'package:day1/study_case_1.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  String _inputData = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        _inputData = _controller.text;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Study Case 3 - Day 2')),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Selamat Datang ${'$_inputData'}di Flutter',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
