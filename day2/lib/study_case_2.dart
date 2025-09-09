a15081fe57025d201524136838f0197d51266af4
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-10 01:20:45 +0700

    perbaiki layout container

--- day2/lib/study_case_2.dart
+++ day2/lib/study_case_2.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+/*
+🧑‍💻 Study Kasus 2
+Buat tampilan page baru dengan kotak berwarna biru serta ukuran height = 100 width = 100 dan kotak 2 x 2, menggunakan Container dengan padding dan margin serta column dan row.
+
+Hasil:
+
+Kotak biru dengan ukuran 100 x 100 sebanyak 4 kotak.
+Ada jarak dari tepi layar.
+*/
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -16,7 +26,68 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Study Case 2 - Day 2'),),
+        appBar: AppBar(title: Text('Study Case 2 - Day 2')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.black26,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.amber,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.blue,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: const Color.fromARGB(255, 71, 115, 129),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.green,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.red,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
