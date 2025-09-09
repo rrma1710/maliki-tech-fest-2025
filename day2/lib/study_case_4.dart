72f333e73ee408446ab4f5e9b818ba20462e4748
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-10 01:54:37 +0700

    add file study case 4

---
+++ day2/lib/study_case_4.dart
@@ -0,0 +1,20 @@
+import 'package:day2/study_case_3.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
