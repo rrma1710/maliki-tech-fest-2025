0aa02da4da6ace3cc619c0fc1b0e68d8976adee0
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-10 01:51:52 +0700

    upload gambar pada container

---
+++ day2/lib/study_case_3.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Study Case 2 - Day 2')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.black26,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Image.asset(
+                            'assets/gunung1.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                          color: Colors.amber,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Image.asset(
+                            'assets/gunung2.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                          color: Colors.blue,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: const Color.fromARGB(255, 71, 115, 129),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Image.network(
+                            'https://ibeka.or.id/wp-content/uploads/2023/10/tanah-lot-temple-bali-island-indonesia_335224-394.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                          color: Colors.green,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Image.network(
+                            'https://tidunglagoon.com/wp-content/uploads/2025/03/laoot.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                          color: Colors.red,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
