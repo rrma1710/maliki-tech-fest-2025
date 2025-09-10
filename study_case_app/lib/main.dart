f4234a91aa29e9831079e9069f50a8aefea5e06e
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-11 00:24:21 +0700

    update data

---
+++ study_case_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Study Case 1 - Day 1')),
+        body: Center(child: Text('Selamat Datang di Flutter')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
