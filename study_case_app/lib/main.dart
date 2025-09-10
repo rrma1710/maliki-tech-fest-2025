1106fd0a7eeff8a36b1bf548e74cddb4ed91a01d
R. Rojab Maulana Akbar <57118028+rrma1710@users.noreply.github.com>
2025-09-11 02:52:31 +0700

    study case app

--- study_case_app/lib/main.dart
+++ study_case_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:study_case_app/pagesecond.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +11,99 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Study Case 1 - Day 1')),
-        body: Center(child: Text('Selamat Datang di Flutter')),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
+      title: 'Coba',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const StudyCaseApp(title: 'Demo'),
+    );
+  }
+}
+
+class StudyCaseApp extends StatefulWidget {
+  final dynamic title;
+
+  const StudyCaseApp({super.key, required this.title});
+
+  @override
+  State<StudyCaseApp> createState() => _StudyCaseAppState();
+}
+
+class _StudyCaseAppState extends State<StudyCaseApp> {
+  final TextEditingController _controller = TextEditingController();
+  String _inputText = '';
+  int counter = 1;
+  int setDefault = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Study Case Day - $setDefault')),
+      body: Column(
+        children: [
+          Center(child: Text('Selamat Datang $_inputText di Flutter!')),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+              onChanged: (value) {
+                setState(() {
+                  _inputText = _controller.text;
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hint: Text('Pengguna'),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => const PageSecond(),
+                ),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  12,
+                ), // Ubah radius sesuai kebutuhan
+              ),
+              minimumSize: Size(
+                80,
+                48,
+              ), // Membuat button kotak lebar penuh dan tinggi 48
+            ),
+            child: Text('Masuk'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            setDefault = counter++;
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
---
+++ study_case_app/lib/pagesecond.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+// void main(List<String> args) {
+//   runApp(const PageSecond());
+// }
+
+class PageSecond extends StatefulWidget {
+  const PageSecond({super.key});
+
+  @override
+  State<PageSecond> createState() => _PageSecondState();
+}
+
+class _PageSecondState extends State<PageSecond> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Study Case Day - 2')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.black26,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.amber,
+                          height: 100,
+                          width: 100,
+                          child: Image.asset(
+                            'assets/gunung1.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.blue,
+                          height: 100,
+                          width: 100,
+                          child: Image.asset(
+                            'assets/gunung2.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: const Color.fromARGB(255, 71, 115, 129),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.green,
+                          height: 100,
+                          width: 100,
+                          child: Image.network(
+                            'https://ibeka.or.id/wp-content/uploads/2023/10/tanah-lot-temple-bali-island-indonesia_335224-394.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.red,
+                          height: 100,
+                          width: 100,
+                          child: Image.network(
+                            'https://tidunglagoon.com/wp-content/uploads/2025/03/laoot.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
